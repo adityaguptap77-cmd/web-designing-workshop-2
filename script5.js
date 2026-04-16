8984510eb65d29d185d6e40299d97b9a6db6b6a7
adityaguptap77-cmd <adityaguptap77@gmail.com>
2026-04-16 20:51:24 +0530

    Update script5.js

--- script5.js
+++ script5.js
@@ -10,17 +10,13 @@
             alert("Please fill all fields");
             return;
         }
-
         employees.push({ name, id, salary, dept });
-
         alert("Employee Added!");
-
         document.getElementById("name").value = "";
         document.getElementById("id").value = "";
         document.getElementById("salary").value = "";
         document.getElementById("dept").value = "";
     }
-
     function displayAll() {
         let output = "<h3>All Employees</h3>";
         employees.forEach(emp => {
@@ -28,24 +24,19 @@
         });
         document.getElementById("output").innerHTML = output;
     }
-
     function filterSalary() {
         let output = "<h3>Employees with Salary > ₹50,000</h3>";
         let filtered = employees.filter(emp => emp.salary > 50000);
-
         filtered.forEach(emp => {
             output += `Name: ${emp.name} | Salary: ₹${emp.salary}<br>`;
         });
-
         document.getElementById("output").innerHTML = output;
     }
-
     function totalSalary() {
         let total = employees.reduce((sum, emp) => sum + emp.salary, 0);
         document.getElementById("output").innerHTML =
             `<h3>Total Salary: ₹${total}</h3>`;
     }
-
     function averageSalary() {
         if (employees.length === 0) return;
 
@@ -55,19 +46,14 @@
         document.getElementById("output").innerHTML =
             `<h3>Average Salary: ₹${avg.toFixed(2)}</h3>`;
     }
-
     function countDept() {
         let deptCount = {};
-
         employees.forEach(emp => {
             deptCount[emp.dept] = (deptCount[emp.dept] || 0) + 1;
         });
-
         let output = "<h3>Employee Count by Department</h3>";
-
         for (let dept in deptCount) {
             output += `${dept}: ${deptCount[dept]}<br>`;
         }
-
         document.getElementById("output").innerHTML = output;
     }
